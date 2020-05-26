8fbbb49e28ff880bad43ad2995c6f790be15701b
Stanley Masinde <stanleyloren@gmail.com>
2020-05-26 20:19:51 +0300

    Transfered the layout from opensource254/WebTempate. It still has not styling

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,117 @@
 <template>
-  <div>
+  <main>
+    <div>
+      <nav
+        class="navbar navbar-light navbar-expand-md fixed-top navigation-clean-button"
+      >
+        <div class="container">
+          <a class="navbar-brand" href="#">opensource254</a
+          ><button
+            class="navbar-toggler"
+            data-toggle="collapse"
+            data-target="#navcol-1"
+          >
+            <span class="sr-only">Toggle navigation</span
+            ><span class="navbar-toggler-icon"></span>
+          </button>
+          <div id="navcol-1" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav mr-auto">
+              <li class="nav-item" role="presentation">
+                <a class="nav-link active" href="#">First Item</a>
+              </li>
+              <li class="nav-item" role="presentation">
+                <a class="nav-link active" href="#">Second Item</a>
+              </li>
+              <li class="dropdown nav-item">
+                <a
+                  class="dropdown-toggle nav-link"
+                  data-toggle="dropdown"
+                  aria-expanded="false"
+                  href="#"
+                  >Dropdown
+                </a>
+                <div class="dropdown-menu" role="menu">
+                  <a class="dropdown-item" role="presentation" href="#"
+                    >First Item</a
+                  ><a class="dropdown-item" role="presentation" href="#"
+                    >Second Item</a
+                  ><a class="dropdown-item" role="presentation" href="#"
+                    >Third Item</a
+                  >
+                </div>
+              </li>
+            </ul>
+            <span class="navbar-text actions">
+              <a
+                class="btn btn-light text-truncate shadow-sm pulse animated infinite action-button"
+                role="button"
+                href="#"
+                style="filter: saturate(116%);"
+                >join us</a
+              ></span
+            >
+          </div>
+        </div>
+      </nav>
+    </div>
+    <!-- End: Navigation with Button -->
     <nuxt />
-  </div>
+    <footer>
+      <div class="row">
+        <div class="col-sm-6 col-md-4 footer-navigation">
+          <h3></h3>
+          <p class="links">
+            <a href="#">Home</a><strong> · · · </strong><a href="#">About</a
+            ><strong> · </strong><a href="#">Faq</a><strong> · </strong
+            ><a href="#">Contact</a>
+          </p>
+          <p class="company-name">opensource kenya © 2020</p>
+        </div>
+        <div class="col-sm-6 col-md-4 footer-contacts">
+          <div>
+            <span
+              class="fa fa-map-marker footer-contacts-icon"
+              style="color: rgb(18,149,138);"
+            >
+            </span>
+            <p><span class="new-line-span">kenya</span></p>
+          </div>
+          <div>
+            <i
+              class="fa fa-phone footer-contacts-icon"
+              style="color: rgb(18,149,138);"
+            ></i>
+            <p class="footer-center-info email text-left">+254</p>
+          </div>
+          <div>
+            <i
+              class="fa fa-envelope footer-contacts-icon"
+              style="color: rgb(18,149,138);"
+            ></i>
+            <p><a href="#" target="_blank">support@opensource.com</a></p>
+          </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="col-md-4 footer-about">
+          <h4>About the opensource</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce
+            euismod convallis velit, eu auctor lacus vehicula sit amet.
+          </p>
+          <div class="social-links social-icons">
+            <a href="#"
+              ><i class="fa fa-facebook" style="color: rgb(18,149,138);"></i></a
+            ><a href="#"
+              ><i class="fa fa-twitter" style="color: rgb(18,149,138);"></i></a
+            ><a href="#"
+              ><i class="fa fa-linkedin" style="color: rgb(18,149,138);"></i
+            ></a>
+            <a href="#"
+              ><i class="fa fa-github" style="color: rgb(18,149,138);"></i
+            ></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </main>
 </template>
-
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,63 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        opensource254
-      </h1>
-      <h2 class="subtitle">
-        Welcome to opensource254. We are about the Kenyan techie interested in
-        contributing to opensource and other projects. We give you a chance to
-        increase your collaboration skills. Visit our about page for more.
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <!-- Start: Features Boxed -->
+  <div class="features-boxed">
+    <div class="container">
+      <!-- Start: Intro -->
+      <div class="intro">
+        <h2 class="text-center">About us</h2>
+        <p class="text-center">
+          Nunc luctus in metus eget fringilla. Aliquam sed justo ligula.
+          Vestibulum nibh erat, pellentesque ut laoreet vitae.
+        </p>
+      </div>
+      <!-- End: Intro -->
+      <!-- Start: Features -->
+      <div class="row justify-content-center features">
+        <div class="col-sm-6 col-md-5 col-lg-4 item">
+          <div class="box">
+            <h3 data-aos="fade" class="name">Works everywhere</h3>
+            <p class="description">
+              Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus.
+              Praesent aliquam in tellus eu.
+            </p>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-5 col-lg-4 item">
+          <div class="box">
+            <h3 data-aos="fade" class="name">Always available</h3>
+            <p class="description">
+              Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus.
+              Praesent aliquam in tellus eu.
+            </p>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-5 col-lg-4 item">
+          <div class="box">
+            <h3 data-aos="fade" class="name">Customizable</h3>
+            <p class="description">
+              Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus.
+              Praesent aliquam in tellus eu.
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- End: Features -->
+    </div>
+    <!-- End: Features Boxed -->
+    <!-- Start: Features Boxed -->
+    <div class="features-boxed" style="background-color: rgb(255,255,255);">
+      <div class="container" style="background-color: #ffffff;">
+        <!-- Start: Intro -->
+        <div class="intro">
+          <h2 class="text-center">About us</h2>
+          <p class="text-center">
+            Nunc luctus in metus eget fringilla. Aliquam sed justo ligula.
+            Vestibulum nibh erat, pellentesque ut laoreet vitae.
+          </p>
+        </div>
+        <!-- End: Intro -->
       </div>
     </div>
   </div>
+  <!-- End: Features Boxed -->
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
